42480118fd63af3e41506c5147f7ffddba774447
Brendan Metcalfe <bgmetcalfe@gmail.com>
2016-12-17 02:22:48 -0500

    notes

--- pomodoro.js
+++ pomodoro.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+  (function myPomodoro () {
+    "use strict";
+    var alarmAudio = new Audio('');
+    function beep() {
+      alarmAudio.play();
+    }
 
+    // https://codepen.io/sevilayha/pen/zbnhm
+    // https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+
+  }());
 });
